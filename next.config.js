6da0c4b8772e6d323fc58ddf397abad1ae5f932a
staraccept <support@staraccept.com>
2024-12-30 11:17:05 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add your other config here
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
