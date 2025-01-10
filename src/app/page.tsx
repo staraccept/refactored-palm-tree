b65f04f716d4d4f2f53806fa924b9e8f5c68ea9a
staraccept <support@staraccept.com>
2025-01-09 23:50:08 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -271,7 +271,7 @@ export default function Home() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-2D18CMVZEF');
+              gtag('config', 'G-2D18CMVZEF'); </script>
             `,
           }}
         />
